437b9df124d8de850fad45c699f68e7024d61a49
Lin Juniper <linjuniper@LindeMacBook-Air.local>
2020-06-16 00:50:43 +0800

    full commit

--- js/all.js
+++ js/all.js
@@ -3,31 +3,62 @@ var galleryTop = new Swiper(".gallery-top", {
   spaceBetween: 10,
   loop: true,
   loopedSlides: 3, //looped slides should be the same
+  autoplay: {
+    delay: 3000,
+  },
 });
 
-$(function(){
-    lineTypeAInit();
-    bindThumbnailEvent();
-})
+$(function () {
+  lineTypeAInit();
+  lineTypeBInit();
+  lineTypeCInit();
+  bindThumbnailEvent();
+  swiperImageInit();
+  modeToggler();
+});
 
-function lineTypeAInit(){
-  let lineTypeA = $('.typeA');
-  if(lineTypeA.length){
-     $('.typeA').addClass('init');
+function lineTypeAInit() {
+  let lineTypeA = $(".typeA");
+  if (lineTypeA.length) {
+    $(".typeA").addClass("init");
   }
 }
 
-function bindThumbnailEvent(){
-  let galleryThumbsItem = $('.gallery-thumbs li');
-  if(galleryThumbsItem.length){
-    galleryThumbsItem.each(function(){
-        let _this = $(this);
-        let itemIndex = _this.index();
-        _this.click(function(){
-          galleryTop.slideTo(itemIndex);
-        })
-    })
+function lineTypeBInit() {
+  let lineTypeB = $(".typeB");
+  if (lineTypeB.length) {
+    $(".typeB").addClass("init");
   }
 }
 
+function lineTypeCInit() {
+  let lineTypeC = $(".typeC");
+  if (lineTypeC.length) {
+    $(".typeC").addClass("init");
+  }
+}
 
+function bindThumbnailEvent() {
+  let galleryThumbsItem = $(".gallery-thumbs li");
+  if (galleryThumbsItem.length) {
+    galleryThumbsItem.each(function () {
+      let _this = $(this);
+      let itemIndex = _this.index();
+      _this.click(function () {
+        galleryTop.slideTo(itemIndex);
+      });
+    });
+  }
+}
+
+function swiperImageInit() {
+  let swiperImage = $(".gallery-top");
+  if (swiperImage.length) {
+    $(".gallery-top").addClass("init");
+  }
+}
+
+$(".light-mode").click(function (e) {
+  e.preventDefault();
+  $("body").toggleClass("lightMode-active");
+});
